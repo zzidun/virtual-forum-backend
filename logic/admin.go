930ffb56537493d3bc285f7c854c06989bcafa21
zzidun <zzidun@outlook.com>
2022-05-22 04:29:15 +0800

    admin update

--- logic/admin.go
+++ logic/admin.go
@@ -1,6 +1,8 @@
 package logic
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"zzidun.tech/vforum0/dao"
 )
@@ -38,6 +40,36 @@ func UserCheckPerm(userId uint, code AdminPermCode) (valid uint, err error) {
 	return
 }
 
-func AdminList(left int, right int) (adminList []*gin.H, err error) {
+func AdminList(left int, right int) (adminList *gin.H, err error) {
+	admins, totNum, curNum, err := dao.AdminQuery(left, right)
+	if err != nil {
+		return
+	}
+
+	var adminListData []*gin.H
+
+	for _, admin := range admins {
+
+		user, err := dao.UserQueryById(admin.UserId)
+		if err != nil {
+			continue
+		}
+
+		adminListData = append(adminListData, &gin.H{
+			"userid":       fmt.Sprintf("%d", user.ID),
+			"username":     user.Name,
+			"adminid":      fmt.Sprintf("%d", admin.ID),
+			"adminperm":    fmt.Sprintf("%d", admin.AdminPerm),
+			"banperm":      fmt.Sprintf("%d", admin.BanPerm),
+			"categoryperm": fmt.Sprintf("%d", admin.CategoryPerm),
+		})
+	}
+
+	adminList = &gin.H{
+		"tot":  fmt.Sprintf("%d", totNum),
+		"cur":  fmt.Sprintf("%d", curNum),
+		"list": adminListData,
+	}
+
 	return
 }
